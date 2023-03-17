3bb415c579ad96965d646fdbd2653b71b875c4dd
Ricky <rzh4321@gmail.com>
2023-03-17 18:02:40 -0400

    created rows of buttons in html

--- calculator.js
+++ calculator.js
@@ -24,3 +24,23 @@ function operate(operator, a, b) {
     if (operator == '/') return divide(a, b);
 
 }
+
+function createRows() {
+    const arr = [7,8,9,'<=','CLR',4,5,6,'X','/',1,2,3,'+','-',0,'.','+/-',`P`,'='];
+    let ind = 0;
+    for (let r = 0; r < 4; ++r) {
+        let row = document.createElement('div');
+        row.classList.add('button-row');
+        for (let col = 0; col < 5; ++col) {
+            let button = document.createElement('div');
+            button.classList.add('button');
+            button.textContent = arr[ind];
+            ++ind;
+            row.append(button);
+        }
+        buttons.append(row);
+    }
+
+}
+
+createRows();
