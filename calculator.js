a484dca21430875325a0ac8a43fe180b308cc2be
Ricky <rzh4321@gmail.com>
2023-03-18 16:28:34 -0400

    created pressBack, not tested

--- calculator.js
+++ calculator.js
@@ -81,3 +81,16 @@ function pressSymbol(symbol="") {
         }
     }
 }
+
+function pressBack() {
+    if (operand1 != "ERROR") { 
+        if (operand2 != "") {
+            operand2 = operand2.slice(0, operand2.length-1);
+            display.textContent = operand2;
+        }
+        else if (operand1 != "" && operator == "") {
+            operand1 = operand1.slice(0, operand1.length-1);
+            display.textContent = operand1;
+        }
+    }
+}
